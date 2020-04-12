eed72ca12983a463355aa01d16236ac1474a61bd
tannineo <tannineo@outlook.com>
2020-04-12 14:20:29 +0100

    chdir

--- supporting_scripts/combine_csv.py
+++ supporting_scripts/combine_csv.py
@@ -1,8 +1,10 @@
 import os
 import glob
 import pandas as pd
-os.chdir(os.getcwd()+"/data/soldiers")
+os.chdir(os.getcwd() + "/data/soldiers")
 extension = 'csv'
 all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
-combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
+
+os.chdir(os.getcwd() + "/..")
 combined_csv.to_csv("combined_soldiers.csv", index=False, encoding='utf-8-sig')
