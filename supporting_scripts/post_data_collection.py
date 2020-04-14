ce530fc88141b2dee1754e781b8c945fa2fd3898
tannineo <tannineo@outlook.com>
2020-04-14 05:53:27 +0100

    update

--- supporting_scripts/post_data_collection.py
+++ supporting_scripts/post_data_collection.py
@@ -10,11 +10,10 @@ import json
 import pandas as pd
 import datetime as dt
 
-
 CURRENT_WORKDIR = os.getcwd()
 
-class Preprocess(object):
 
+class Preprocess(object):
     def __init__(self):
         self.veteran_stats = dict()
         self.civilian_stats = dict()
@@ -37,21 +36,26 @@ class Preprocess(object):
                 cleaned_tweet = cleaned_tweet.replace("#", "")
                 number_of_words = len(cleaned_tweet)
                 if data_type == "soldiers":
-                    self.veteran_stats['words'] = number_of_words if self.veteran_stats.get('words') is None else (self.veteran_stats['words'] + number_of_words)
+                    self.veteran_stats[
+                        'words'] = number_of_words if self.veteran_stats.get(
+                            'words') is None else (
+                                self.veteran_stats['words'] + number_of_words)
                     self.cleaned_veteran_tweets.append(cleaned_tweet)
                 else:
-                    self.civilian_stats['words'] = number_of_words if self.civilian_stats.get(
-                        'words') is None else (self.civilian_stats['words'] + number_of_words)
+                    self.civilian_stats[
+                        'words'] = number_of_words if self.civilian_stats.get(
+                            'words') is None else (
+                                self.civilian_stats['words'] + number_of_words)
                     self.cleaned_civilian_tweets.append(cleaned_tweet)
 
     def check_profanity(self, tweet, data_type):
         if predict([tweet]) == 1:
             if data_type == 'soldiers':
                 self.veteran_stats['curses'] = 1 if self.veteran_stats.get(
-                    'curses') is None else(self.veteran_stats['curses'] + 1)
+                    'curses') is None else (self.veteran_stats['curses'] + 1)
             else:
                 self.civilian_stats['curses'] = 1 if self.civilian_stats.get(
-                    'curses') is None else(self.civilian_stats['curses'] + 1)
+                    'curses') is None else (self.civilian_stats['curses'] + 1)
 
     def aggregate_stats_from_df(self, vet_df, civ_df):
         self.veteran_stats['retweets'] = vet_df.retweets_count.sum()
@@ -68,23 +72,40 @@ class Preprocess(object):
         number_of_urls = token_string.count('$URL$')
         number_of_mentions = token_string.count('$MENTION$')
         if data_type == "soldiers":
-            self.veteran_stats['hashtags'] = number_of_hashtags if self.veteran_stats.get(
-                'hashtags') is None else (int(self.veteran_stats['hashtags']) + number_of_hashtags)
-            self.veteran_stats['emojis'] = number_of_emojis if self.veteran_stats.get(
-                'emojis') is None else (self.veteran_stats['emojis'] + number_of_emojis)
-            self.veteran_stats['urls'] = number_of_urls if self.veteran_stats.get(
-                'urls') is None else (self.veteran_stats['urls'] + number_of_urls)
-            self.veteran_stats['mentions'] = number_of_mentions if self.veteran_stats.get(
-                'mentions') is None else (self.veteran_stats['mentions'] + number_of_mentions)
+            self.veteran_stats[
+                'hashtags'] = number_of_hashtags if self.veteran_stats.get(
+                    'hashtags') is None else (
+                        int(self.veteran_stats['hashtags']) +
+                        number_of_hashtags)
+            self.veteran_stats[
+                'emojis'] = number_of_emojis if self.veteran_stats.get(
+                    'emojis') is None else (self.veteran_stats['emojis'] +
+                                            number_of_emojis)
+            self.veteran_stats[
+                'urls'] = number_of_urls if self.veteran_stats.get(
+                    'urls') is None else (self.veteran_stats['urls'] +
+                                          number_of_urls)
+            self.veteran_stats[
+                'mentions'] = number_of_mentions if self.veteran_stats.get(
+                    'mentions') is None else (self.veteran_stats['mentions'] +
+                                              number_of_mentions)
         else:
-            self.civilian_stats['hashtags'] = number_of_hashtags if self.civilian_stats.get(
-                'hashtags') is None else(self.civilian_stats['hashtags'] + number_of_hashtags)
-            self.civilian_stats['emojis'] = number_of_emojis if self.civilian_stats.get(
-                'emojis') is None else(self.civilian_stats['emojis'] + number_of_emojis)
-            self.civilian_stats['urls'] = number_of_urls if self.civilian_stats.get(
-                'urls') is None else(self.civilian_stats['urls'] + number_of_urls)
-            self.civilian_stats['mentions'] = number_of_mentions if self.civilian_stats.get(
-                'mentions') is None else(self.civilian_stats['mentions'] + number_of_mentions)
+            self.civilian_stats[
+                'hashtags'] = number_of_hashtags if self.civilian_stats.get(
+                    'hashtags') is None else (self.civilian_stats['hashtags'] +
+                                              number_of_hashtags)
+            self.civilian_stats[
+                'emojis'] = number_of_emojis if self.civilian_stats.get(
+                    'emojis') is None else (self.civilian_stats['emojis'] +
+                                            number_of_emojis)
+            self.civilian_stats[
+                'urls'] = number_of_urls if self.civilian_stats.get(
+                    'urls') is None else (self.civilian_stats['urls'] +
+                                          number_of_urls)
+            self.civilian_stats[
+                'mentions'] = number_of_mentions if self.civilian_stats.get(
+                    'mentions') is None else (self.civilian_stats['mentions'] +
+                                              number_of_mentions)
 
     def process_data(self):
         pass
@@ -92,9 +113,12 @@ class Preprocess(object):
     def get_combined_file(self, folder_name="soldiers", force_rewrite=False):
         os.chdir("{}/data/{}".format(CURRENT_WORKDIR, folder_name))
         all_filenames = [i for i in glob.glob('*.{}'.format('csv'))]
-        if not os.path.exists("combined_{}.csv".format(folder_name)) or force_rewrite:
+        if not os.path.exists(
+                "combined_{}.csv".format(folder_name)) or force_rewrite:
             combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
-            combined_csv.to_csv("combined_{}.csv".format(folder_name), index=False, encoding='utf-8-sig')
+            combined_csv.to_csv("combined_{}.csv".format(folder_name),
+                                index=False,
+                                encoding='utf-8-sig')
             print("combined_{}.csv saved".format(folder_name))
         else:
             print("File exists. Skipping. Use force_rewrite to overwrite.")
@@ -113,8 +137,9 @@ class Preprocess(object):
             self.pre_process_data(vet_tweets[i], "soldiers")
         self.cleaned_vet_tweet_df = pd.DataFrame(
             {'tweets': self.cleaned_veteran_tweets})
-        self.cleaned_vet_tweet_df.to_csv(
-            'cleaned_vet_tweet_df.csv', header=False, mode='a')
+        self.cleaned_vet_tweet_df.to_csv('cleaned_vet_tweet_df.csv',
+                                         header=False,
+                                         mode='a')
         print("veteran stats: {}".format(self.veteran_stats))
         print('cleaned_vet_tweet_df.csv created')
         with open('{}.txt'.format(dt.datetime.utcnow()), 'w') as file:
@@ -128,12 +153,14 @@ class Preprocess(object):
                 self.pre_process_data(vet_tweets[i], "civilians")
         self.cleaned_civ_tweet_df = pd.DataFrame(
             {'tweets': self.cleaned_civilian_tweets})
-        self.cleaned_civ_tweet_df.to_csv(
-            'cleaned_civ_tweet_df.csv', header=False, mode='a')
+        self.cleaned_civ_tweet_df.to_csv('cleaned_civ_tweet_df.csv',
+                                         header=False,
+                                         mode='a')
         print("civilian stats: {}".format(self.civilian_stats))
         print('cleaned_civ_tweet_df.csv created')
         with open('{}.txt'.format(dt.datetime.utcnow()), 'w') as file:
             file.write(json.dumps(self.civilian_stats))
+
 
 if __name__ == "__main__":
     # start_time = time.time()
@@ -144,12 +171,14 @@ if __name__ == "__main__":
     # print("Vet Shape: {}".format(vet_df.shape))
     # pr.clean_veteran_tweet_and_create_df(vet_df)
     # print("--- %s seconds ---" % (time.time() - start_time))
-    
 
     start_time = time.time()
     pr = Preprocess()
     civ_df = pr.get_dataframe(data_type="civilians")
-    civ_df = civ_df[['tweet', 'time', 'photos', 'replies_count', 'retweets_count', 'likes_count', 'retweet']]
+    civ_df = civ_df[[
+        'tweet', 'time', 'photos', 'replies_count', 'retweets_count',
+        'likes_count', 'retweet'
+    ]]
     civ_df = civ_df[(civ_df['retweet'] == False)]
     print("Civ Shape: {}".format(civ_df.shape))
 
